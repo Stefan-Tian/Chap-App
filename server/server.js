6f571317ca630e8d7e25cebe75e193a8978ae5c5
Stefan Tien <stefan55831@gmail.com>
2017-08-13 13:20:44 +0800

    Add connect & disconnect eevents

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public")
 const port = process.env.PORT || 4222
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected");
+  });
+});
+
+
+
+server.listen(port, () => {
   console.log("Successfully connected to localhost:4222");
 });
