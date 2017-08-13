7fada65246571c0a16e982d032bea69fe92f10e5
Stefan Tien <stefan55831@gmail.com>
2017-08-13 18:37:18 +0800

    Add newMessage & createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+const socket = io();
+
+socket.on("connect", function () {
+  console.log("Connected to server");
+
+  socket.emit("createMessage", {
+    from: "Stefan",
+    text: "Yes!"
+  });
+
+  socket.on("disconnect", function () {
+    console.log("Disconnected to server");
+  });
+});
+
+
+
+socket.on("newMessage", function (message) {
+  console.log(message);
+});
--- server/server.js
+++ server/server.js
@@ -14,12 +14,20 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "damon",
+    text: "Marry me",
+    sendAt: new Date().getTime()
+  });
+
+  socket.on("createMessage", (message) => {
+    console.log(message);
+  });
+
   socket.on("disconnect", () => {
     console.log("Disconnected");
   });
 });
-
-
 
 server.listen(port, () => {
   console.log("Successfully connected to localhost:4222");
