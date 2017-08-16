901762e3bcb7f981e258454501fcfc6364d86c85
Stefan Tien <stefan55831@gmail.com>
2017-08-16 13:14:06 +0800

    Add autoscroll to messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  const messages = $("#messages");
+  const newMessage = messages.children("li:last-child");
+  // Heights
+  const clientHeight = messages.prop("clientHeight");
+  const scrollTop = messages.prop("scrollTop");
+  const scrollHeight = messages.prop("scrollHeight");
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on("connect", function () {
   console.log("Connected to server");
 });
@@ -14,6 +30,7 @@ socket.on("newMessage", function (message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 socket.on("disconnect", function () {
@@ -43,6 +60,7 @@ socket.on("newLocationMessage", function (message) {
   });
 
   $(messages).append(html);
+  scrollToBottom();
 });
 
 const locButton = $("#send-location");
